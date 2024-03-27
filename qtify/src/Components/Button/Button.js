c5715aaf450fbd3b83835edbb0ca725bf3fd99e3
Dhananjay-3270 <shinde3270@gmail.com>
2024-03-27 22:58:13 +0530

    P4

--- qtify/src/Components/Button/Button.js
+++ qtify/src/Components/Button/Button.js
@@ -2,12 +2,6 @@ import React from "react";
 import styles from "./Button.module.css"
 
 function Button() {
-  return (
-    <>
-    <div className={styles.div   } id="text">
-      <button className={styles.Button}>Give Feedback</button>
-      </div>
-    </>
-  );
+  return  <button className={styles.Button}>Give Feedback</button>
 }
 export default Button;
