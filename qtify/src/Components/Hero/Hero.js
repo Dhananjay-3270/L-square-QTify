f7827636fe80aad1ab6b11b6240c5bb1b9153549
Dhananjay-3270 <shinde3270@gmail.com>
2024-03-28 12:45:07 +0530

    H3_diff_approach

---
+++ qtify/src/Components/Hero/Hero.js
@@ -0,0 +1,20 @@
+import styles from "./Hero.module.css";
+import headphone from "./headphone.png";
+function Hero() {
+  return (
+    <>
+      <div className={styles.hero}>
+        <div className={styles.heading}>
+          <h1>100 Thousand Songs, ad-free</h1>
+          <h1>Over thousands podcast episodes</h1>
+          </div>
+          <div>
+            <img src={headphone} width={212} alt="headphone" />
+          </div>
+       
+      </div>
+    </>
+  );
+}
+
+export default Hero;
--- qtify/src/Pages/Homepage/Homepage.js
+++ qtify/src/Pages/Homepage/Homepage.js
@@ -4,22 +4,21 @@ import {Link} from "react-router-dom"
 import Navbar from "../../Components/Navbar/Navbar"
 import { Box } from "@mui/system";
 import styles from "../../Components/Hero/Hero.module.css"
+import Hero from "../../Components/Hero/Hero";
 function Homepage() {
   return (
     <div>
 <Navbar/>
-<Box  display="flex"
+{/* <Box  display="flex"
       flexDirection="column"
       justifyContent="space-between"
       minHeight="100vh"
       className={styles.box}>
 <Box className={styles.hero}>
-
-
 </Box>
 <p className={styles.heading}>100 Thousand Songs, ad-free Over thousands podcast episodes</p>
-
-</Box>
+</Box> */}
+<Hero/>
     </div>
   );
 }
