31c9b436e2baa0dfb9cb5f9ca62f14275fec30ff
Dhananjay-3270 <shinde3270@gmail.com>
2024-04-11 18:51:13 +0530

    Position of buttons changed

--- qtify/src/Components/Carousel/Rightarrow/Rightarrow.js
+++ qtify/src/Components/Carousel/Rightarrow/Rightarrow.js
@@ -1,10 +1,13 @@
 import { ReactComponent as Right } from "./Right.svg";
 import { useSwiper } from "swiper/react";
+import styles from "./Rightarrow.module.css"
 function Rightarrow() {
   const swiper = useSwiper();
   return (
     <>
+    <div className={styles.right}>
       <Right onClick={() => swiper.slideNext()} />
+      </div>
     </>
   );
 }
